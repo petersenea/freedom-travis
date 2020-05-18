a78280dabe3ecf1e23af453215b8621ce18e4033
petersenea <Petersenea12@gmail.com>
2020-05-18 12:12:11 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import EmailForm from './components/EmailForm';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/email" component={EmailForm} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
